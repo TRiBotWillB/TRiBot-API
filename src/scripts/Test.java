8ac03b2e47897c6a08185784d1205cd14d8a6945
Will Baker <willbaker698@gmail.com>
2019-11-08 18:15:04 +0000

    Inital Commit

---
+++ src/scripts/Test.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import org.tribot.script.Script;
+
+public class Test extends Script {
+
+    public void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("SCRIPT RUN");
+    }
+}
